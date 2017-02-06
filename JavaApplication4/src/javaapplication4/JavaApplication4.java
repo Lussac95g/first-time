dbf2b693585d7b63197b305e281faaa07b2a569c
Lussac95g <isaac.perez9595@gmail.com>
2017-02-06 11:21:28 -0500

    v.1.0
    Modificado

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -16,6 +16,7 @@ public class JavaApplication4 {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("/n Hola Dennis");
         // TODO code application logic here
     }
     
